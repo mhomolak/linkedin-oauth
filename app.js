802141e0b4aecdf5467900b3cbce152ee56d3ba5
Mike Homolak <mhomolak@yahoo.com>
2016-03-31 14:47:30 -0700

    Login shows your name

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').load();
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -6,7 +7,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var LinkedInStrategy = require('passport-linkedin-oauth2').Strategy;
 var passport = require('passport');
-require('dotenv').load();
+var cookieSession = require('cookie-session');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -25,18 +26,34 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(passport.initialize());
+app.use(passport.session());
 
+app.use(cookieSession({
+  secret: process.env.LINKEDIN_CLIENT_SECRET
+}));
+
+app.get('/auth/linkedin',
+passport.authenticate('linkedin'),
+function(req, res){
+  // The request will be redirected to LinkedIn for authentication, so this
+  // function will not be called.
+});
+
+app.get('/auth/linkedin/callback', passport.authenticate('linkedin', {
+  successRedirect: '/',
+  failureRedirect: '/login'
+}));
 
 passport.use(new LinkedInStrategy({
     clientID: process.env.LINKEDIN_CLIENT_ID,
     clientSecret: process.env.LINKEDIN_CLIENT_SECRET,
     callbackURL: process.env.HOST + "/auth/linkedin/callback",
-    scope: ['r_emailaddress', 'r_basicprofile']
+    scope: ['r_emailaddress', 'r_basicprofile'],
+    state: true
   },
   function(accessToken, refreshToken, profile, done) {
-    done(null, {id: profile.id, displayName: profile.displayName, token: accessToken})
-  }
-));
+    done(null, {id: profile.id, displayName: profile.displayName})
+  }));
 
 // above app.use('/', routes);...
 passport.serializeUser(function(user, done) {
@@ -44,23 +61,13 @@ passport.serializeUser(function(user, done) {
 });
 
 passport.deserializeUser(function(user, done) {
-  done(null, user)
+  done(null, user);
 });
 
 app.use('/', routes);
 app.use('/users', users);
 
-app.get('/auth/linkedin',
-  passport.authenticate('linkedin', { state: 'SOME STATE'  }),
-  function(req, res){
-    // The request will be redirected to LinkedIn for authentication, so this
-    // function will not be called.
-  });
 
-app.get('/auth/linkedin/callback', passport.authenticate('linkedin', {
-  successRedirect: '/',
-  failureRedirect: '/login'
-}));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
